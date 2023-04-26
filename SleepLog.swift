26352aa6ab6614f2abf7ea77324d358a64e0ef74
Claudia <csepko4429@stu.d214.org>
2023-04-26 14:38:10 -0500

    created navigation links, and started Survey page

---
+++ SleepLog.swift
@@ -0,0 +1,16 @@
+//
+//  SleepLog.swift
+//  sleepTracker
+//
+//  Created by Claudia Sepko on 4/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct SleepLog: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
